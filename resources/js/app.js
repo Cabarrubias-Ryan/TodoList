ded17acb91b8f3b43194ae65213660e8c4e541ee
Cabarrubias-Ryan <cabarrubias1002@gmail.com>
2025-01-24 19:49:47 +0800

    TODO List web App using socialite

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title','BARGO - Reservation')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <button>Google</button>
+            </div>
+        </div>
+    </div>
+@endsection
